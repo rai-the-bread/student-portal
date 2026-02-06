0556fc0868e238393199c5059114c209139ace06
Rai Bread <rad@Razmiks-MacBook-Pro.local>
2026-02-06 16:20:42 -0500

    removed piece of code not being used

--- api/index.js
+++ api/index.js
@@ -765,9 +765,3 @@ if (require.main === module) {
     }
   })();
 }
-
-// Export for Vercel serverless
-module.exports = async (req, res) => {
-  await ensureStudentsLoaded();
-  return app(req, res);
-};
